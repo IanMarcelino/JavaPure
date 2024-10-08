10d87987bbec5a018d611971c37fc18d4ddf2cb4
ALUNO K5 <alunok5@acad.unifor.br>
2024-10-08 20:17:51 -0300

    New Ex

---
+++ POO/ExInterface-Jogador/Movimento.java
@@ -0,0 +1,7 @@
+public interface  Movimento {
+    public abstract void defender();
+    public abstract void chutar();
+    public abstract void correr();
+    public abstract void andar();
+    
+}
